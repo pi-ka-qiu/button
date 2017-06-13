a74ffe0225e49493118857acf1c2f7836574bf06
gdmec07150938 <WXJ907468952@outlook.com>
2017-06-13 11:40:01 +0800

    不支持ie9及以下

--- test.js
+++ test.js
@@ -1,39 +1,43 @@
-/**
- * 接收参数
- * color： ‘#fff’
- * r： 5
- * time： 1000
- */
 var ripple_utill = {
-    isEmptyObject: function(obj) {
-        for (let i in obj) { return false }
-        return true
-    },
+        isEmptyObject: function(obj) {
+            for (let i in obj) { return false }
+            return true
+        },
 
-    hasClass: function(ele, className) {
-        return ele.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
-    },
+        hasClass: function(ele, className) {
+            return ele.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+        },
 
-    addClass: function(ele, className) {
-        if (!this.hasClass(ele, className)) { ele.className += " " + className; }
-        console.log('--addClass--')
-    },
+        addClass: function(ele, className) {
+            if (!this.hasClass(ele, className)) { ele.className += " " + className; }
+            console.log('--addClass--')
+        },
 
-    removeClass: function(ele, className) {
-        if (this.hasClass(ele, className)) {
-            var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
-            ele.className = ele.className.replace(reg, '');
+        removeClass: function(ele, className) {
+            if (this.hasClass(ele, className)) {
+                var reg = new RegExp('(\\s|^)' + className + '(\\s|$)');
+                ele.className = ele.className.replace(reg, '');
+            }
+        },
+        browserSupportEvent: function(eventName) {
+            return eventName in window;
         }
-    },
-    browserSupportEvent: function(eventName) {
-        return eventName in window;
     }
-}
+    /**
+     * 接收参数
+     * cName: 'md-button' 
+     * color： ‘#fff’
+     * r： 5
+     * time： 1000 
+     * rippleMultiple: true  是否出现多个水波纹
+     * MaxNum：3 最多出现多少个水波纹
+     */
 Circular.prototype = {
-    r: 5,
+    r: 20,
     cName: 'md-button',
-    color: '#fff',
-    time: 1000
+    color: 'rgba(0, 0, 0, 0.3)',
+    time: '0.5s',
+    MaxNum: 5
 }
 
 function Circular() {
@@ -44,6 +48,15 @@ function Circular() {
     };
     this.setY = function(newValue) {
         this.y = newValue
+    };
+    this.createRipple = function(tagName = 'span') {
+        let span = document.createElement(tagName)
+        ripple_utill.addClass(span, 'md-ripple')
+        span.style.width = 2 * this.r + 'px';
+        span.style.height = 2 * this.r + 'px';
+        span.style.backgroundColor = this.color;
+        span.style.animationDuration = this.time;
+        return span
     }
 }
 
@@ -58,6 +71,7 @@ class Ripple {
             Circular.prototype.cName = obj.cName || Circular.prototype.cName
             Circular.prototype.color = obj.color || Circular.prototype.color
             Circular.prototype.time = obj.time || Circular.prototype.time
+            Circular.prototype.MaxNum = obj.MaxNum || Circular.prototype.MaxNum
         }
     };
     //为所有按钮添加监听，添加span
@@ -68,29 +82,26 @@ class Ripple {
             let circular = new Circular()
                 //将所有按钮设置为相对定位
                 //let t = buttonList[i].style.position = 'relative'
-            let wrapper = this.createRippleNode()
+            let wrapper = this.createRippleWrapper(circular)
             buttonList[i].appendChild(wrapper)
             buttonList[i].addEventListener('click', (event) => {
-                this.reppleClick(event, buttonList[i], circular, this.createRippleChildNode(wrapper));
+                this.reppleClick(event, buttonList[i], circular, this.createRippleChildNode(circular, wrapper));
             })
 
         }
     };
-    createRippleNode(tagName = 'span') {
+    createRippleWrapper(circular) {
         //在button下创建一个div作为wrapper，默认有一个span.
-        let div = document.createElement('div');
+        let div = document.createElement('div')
         ripple_utill.addClass(div, 'md-wrapper')
-        let span = document.createElement(tagName)
-        ripple_utill.addClass(span, 'md-ripple')
-        span.style.width = 2 * Circular.prototype.r + 'px';
-        span.style.height = 2 * Circular.prototype.r + 'px';
+        let span = circular.createRipple('span')
         span.addEventListener('animationend', function(event) {
             ripple_utill.removeClass(this, 'animate')
         })
         div.appendChild(span);
         return div;
     };
-    createRippleChildNode(wrapper) {
+    createRippleChildNode(circular, wrapper) {
         //创建wrapper子节点span时，判断wrapper下是否有span空闲(动画停止)，空闲则重用，所有都没有则新建span。
         let childrenList = wrapper.children;
         for (let i = 0; i < childrenList.length; i++) {
@@ -98,11 +109,8 @@ class Ripple {
             if (flag == null) { //动画已经停止了
                 ripple_utill.addClass(childrenList[i], 'md-ripple')
                 return childrenList[i];
-            } else if (flag != null && i === childrenList.length - 1) { //最后一个span && 动画没有停止
-                let span = document.createElement('span')
-                ripple_utill.addClass(span, 'md-ripple')
-                span.style.width = 2 * Circular.prototype.r + 'px';
-                span.style.height = 2 * Circular.prototype.r + 'px';
+            } else if (flag != null && i === childrenList.length - 1 && circular.MaxNum >= childrenList.length) { //最后一个span && 动画没有停止&& 没有超过最多个数
+                let span = circular.createRipple('span');
                 span.addEventListener('animationend', function(event) {
                     //新建的span在动画结束后，从wrapper中移除
                     wrapper.removeChild(this)
@@ -111,6 +119,7 @@ class Ripple {
                 return span;
             }
         }
+        return childrenList[0];
     };
     reppleClick(event, button, circular, ripple) {
         /*相对于 按钮的点击坐标
@@ -129,15 +138,5 @@ class Ripple {
         ripple.style.left = circular.x + 'px';
         ripple.style.top = circular.y + 'px';
         ripple_utill.addClass(ripple, 'animate')
-            //添加动画后，如果浏览器不支持animationend，使用setTimeout移除
-            //window.setTimeout(function() { removeClass(ripple, 'animate') }, circular.time)
     }
 }
-
-//取得所有按钮
-let test = new Ripple({
-    r: 20,
-    cName: 'md-button',
-    color: '#fff',
-    time: 1000
-})
